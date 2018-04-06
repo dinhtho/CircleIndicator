0d194e70b7146f6a85f09065281716c8e4b25b30
thonguyen <dinhthohcmus@gmail.com>
2018-04-06 10:47:40 +0700

    add ViewPager funtion

--- app/src/main/java/com/example/pcpv/circleindicator/MainActivity.java
+++ app/src/main/java/com/example/pcpv/circleindicator/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pcpv.circleindicator;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
 
 import com.rd.PageIndicatorView;
 
@@ -10,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,9 +27,48 @@ public class MainActivity extends AppCompatActivity {
         }
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), fragmentList);
         viewPager.setAdapter(viewPagerAdapter);
-        PageIndicatorView pageIndicatorView = findViewById(R.id.pageIndicatorView);
-        pageIndicatorView.setViewPager(viewPager);
 
+        setPageIndicator(viewPager);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                Log.i(TAG, "onPageScrolled: " + position);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.i(TAG, "onPageSelected: " + position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                Log.i(TAG, "onPageScrollStateChanged: " + state);
+            }
+        });
 
     }
+
+    private void setPageIndicator(ViewPager viewPager) {
+        PageIndicatorView pageIndicatorView = findViewById(R.id.pageIndicatorView);
+        pageIndicatorView.setViewPager(viewPager);
+    }
+
+    private void setFragmentShown(ViewPager viewPager, int index) {
+        viewPager.setCurrentItem(index);
+    }
+
+    private int getCurrentFragmentShown(ViewPager viewPager) {
+        return viewPager.getCurrentItem();
+    }
+
+    private void disableSwipeInViewPager(ViewPager viewPager) {
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                // override onTough feature
+                return true;
+            }
+        });
+    }
+
 }
